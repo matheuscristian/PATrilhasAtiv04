3e5a71f8114737fbd70646df7d1f470be34bd317
Matheus Cristian <81980814+matheuscristian@users.noreply.github.com>
2025-01-30 08:52:05 -0300

    Update f.cs

--- lista04/f.cs
+++ lista04/f.cs
@@ -6,7 +6,7 @@ public class HelloWorld
     {
         string tem_carteira;
         
-        Console.Write("Voce que acucar ou leite no seu cafe? (Sim/Nao): ");
+        Console.Write("Voce que acucar ou leite no seu cafe?: ");
         tem_carteira = Console.ReadLine().Trim().ToLower();
         
         if (tem_carteira == "acucar" || tem_carteira == "leite") {
