a5f729652017f88b5ba4a678635d975016dc93dd
Matheus Cristian <81980814+matheuscristian@users.noreply.github.com>
2025-01-30 08:51:26 -0300

    Create f.cs

---
+++ lista04/f.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        string tem_carteira;
+        
+        Console.Write("Voce que acucar ou leite no seu cafe? (Sim/Nao): ");
+        tem_carteira = Console.ReadLine().Trim().ToLower();
+        
+        if (tem_carteira == "acucar" || tem_carteira == "leite") {
+            Console.WriteLine("Cafe com adicional preparado!");
+        }
+    }
+}
